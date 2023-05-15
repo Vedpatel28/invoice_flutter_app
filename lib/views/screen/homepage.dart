d822971cf50feb48d9b31a7025326bce66127fa2
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-05-15 20:27:35 +0530

    VP | 15/05/23 | # Home Page UI Only Complete...

--- lib/views/screen/homepage.dart
+++ lib/views/screen/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
@@ -10,9 +11,174 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
+    Size s = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Shopping"),
+        // Shopping
+        title: Text(
+          "Shopping",
+          style: GoogleFonts.varta(
+            textStyle: TextStyle(
+              fontSize: s.height * 0.035,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1,
+            ),
+          ),
+        ),
+        // Icons OF Like / Cart
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite_border_rounded,
+                  color: Colors.red.shade200,
+                  size: s.height * 0.035,
+                ),
+              ),
+              SizedBox(width: s.width * 0.001),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart_rounded,
+                  size: s.height * 0.035,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Stack(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "category",
+                        style: GoogleFonts.tiroGurmukhi(
+                          textStyle: TextStyle(
+                            fontSize: s.height * 0.03,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: s.height * 0.02),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            Container(
+                              height: s.height * 0.045,
+                              width: s.width * 0.4,
+                              color: Colors.cyanAccent,
+                            ),
+                            SizedBox(width: s.width * 0.05),
+                            Container(
+                              height: s.height * 0.05,
+                              width: s.width * 0.4,
+                              color: Colors.cyanAccent,
+                            ),
+                            SizedBox(width: s.width * 0.05),
+                            Container(
+                              height: s.height * 0.05,
+                              width: s.width * 0.4,
+                              color: Colors.cyanAccent,
+                            ),
+                            SizedBox(width: s.width * 0.05),
+                            Container(
+                              height: s.height * 0.05,
+                              width: s.width * 0.4,
+                              color: Colors.cyanAccent,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: s.height * 0.02),
+                      Container(
+                        width: double.infinity,
+                        height: s.height * 0.67,
+                        child: GridView.builder(
+                          itemCount: 10,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 2 / 2,
+                            crossAxisCount: 2,
+                          ),
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: 200,
+                              width: 300,
+                              color: Colors.deepOrange.shade100,
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: s.height * 0.01),
+            Container(
+              height: s.height * 0.07,
+              width: double.infinity,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(30),
+                ),
+                color: Colors.black,
+              ),
+              child: Row(
+                children: [
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.home,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.favorite_border_rounded,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.shopping_cart_rounded,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.account_circle,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
